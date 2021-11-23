4d8a3d809ae6fa14f17c83f602ffe3dc20f802f5
NeGameOnline <89255600+NeGameOnline@users.noreply.github.com>
2021-11-23 23:55:27 +0300

    Update fr_doker2.sh

--- fr_doker2.sh
+++ fr_doker2.sh
@@ -9,6 +9,7 @@ cd /opt/fruitnanny
 openssl req -x509 -sha256 -nodes -days 2650 -newkey rsa:2048 -keyout configuration/ssl/fruitnanny.key -out configuration/ssl/fruitnanny.pem -subj "/C=AU/ST=NSW/L=Sydney/O=MongoDB/OU=root/CN=`hostname -f`"
 
 # Generate apache passwords
+sudo mkdir /etc/nginx/
 sudo sh -c "echo -n 'fr:' >> /etc/nginx/.htpasswd"
 sudo sh -c "openssl passwd -1 "123" -apr1 >> /etc/nginx/.htpasswd"
 
