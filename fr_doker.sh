d6141d2938b6c6e607b51892673892994f9d2e90
NeGameOnline <89255600+NeGameOnline@users.noreply.github.com>
2021-11-23 20:53:11 +0300

    Update fr_doker.sh

--- fr_doker.sh
+++ fr_doker.sh
@@ -20,23 +20,5 @@ sudo usermod -aG docker pi
 # docker-compose
 sudo apt -y install  docker-compose
 
-# Clone repository
-cd /opt
-sudo mkdir fruitnanny
-sudo chown pi:pi fruitnanny
-git clone https://github.com/ivadim/fruitnanny
-
-# Generate certificates
-cd /opt/fruitnanny
-openssl req -x509 -sha256 -nodes -days 2650 -newkey rsa:2048 -keyout configuration/ssl/fruitnanny.key -out configuration/ssl/fruitnanny.pem -subj "/C=AU/ST=NSW/L=Sydney/O=MongoDB/OU=root/CN=`hostname -f`"
-
-# Generate apache passwords
-sudo sh -c "echo -n 'fr:' >> /etc/nginx/.htpasswd"
-sudo sh -c "openssl passwd -1 "123" -apr1 >> /etc/nginx/.htpasswd"
-
 # Reboot system
 sudo shutdown -r -t sec 30
-
-# Run the system with notifications
-# cd /opt/fruitnanny
-# docker-compose -f docker-compose.yml -f docker-compose.local.yml up -d
