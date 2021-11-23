5b0239facc44c576524dac6c09fa9bf89aa6b367
NeGameOnline <89255600+NeGameOnline@users.noreply.github.com>
2021-11-23 21:10:01 +0300

    Update fr_doker1.sh

--- fr_doker1.sh
+++ fr_doker1.sh
@@ -2,9 +2,9 @@
 
 # updates
 sudo apt update
-sudo apt full-upgrade
-sudo apt install rpi-update
-sudo rpi-update -y
+sudo apt full-upgrade -y
+sudo apt install rpi-update -y
+sudo rpi-update
 
 # enable camera
 # sudo raspi-config
@@ -12,7 +12,17 @@ sudo raspi-config nonint do_camera 0
 
 # Install tools
 # git and extra libs
-sudo apt -y install git curl libffi-dev python python-pip
+# sudo apt -y install git curl libffi-dev python python-pip
+sudo apt -y install git curl libffi-dev
+
+# ERROR install python
+sudo apt -y install python-is-python2
+# ERROR install python-pip
+# ERROR Package 'python-pip' has no installation candidate
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+sudo python get-pip.py
+
+
 # docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
